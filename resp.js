fa6ce68fb98e1056a71f6f69d46e38f534b2e650
G Sai Mani <saimanig0077@gmail.com>
2022-10-17 11:20:52 +0530

    Food order web initialize

---
+++ resp.js
@@ -0,0 +1,10 @@
+let a1=document.getElementById("numberincrement");
+// let a2=document.getElementById("btnall");
+// let a3=document.getElementsByClassName("seconder");
+let a2=document.getElementById('linkbtn');
+let i=0;
+document.getElementById("btnall").onclick =function(){a1.innerHTML= i+1;i++;};
+// a3.addEventListener("click",function() {
+//     a1.innerHTML= i+1;
+//     i++;
+// })
